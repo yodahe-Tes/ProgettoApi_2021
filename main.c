7b305a4efd1cf7fab16cbcdb764f55ccf8d95448
yodahe-Tes <yodaheteshome.kumbi@mail.polimi.it>
2021-09-07 21:21:28 +0200

    created function sort, for smaller graphs. optimized dijkstra.

--- main.c
+++ main.c
@@ -2,224 +2,425 @@
 #include <string.h>
 #include <stdlib.h>
 
-#define N_MAX_LINEE 150000
-#define MAX_BUF 2500
-#define MAXGRAF 1500
-#define LUNG_MAX_STRING 2500
+
+#define strToINt(dig,buffer,resultFin)      while ((buffer)[(dig)] > '/'){  (resultFin) = ((resultFin) <<3) + ((resultFin)<<1 ) +(buffer)[(dig)] -'0';   (dig)++;}
+#define MAX_BUF 2400
+#define RED 0
+#define BLACK 1
+#define LUNG_MAX_STRING 2400
 #define INFINITY 9999999
 
-typedef struct DistanzaMin{
-    int indiceGraf;
+typedef struct nodo{
     int SomDistMin;
-    int graduatoria;
-} strDistMin;
+    int indiceGraf;
+    struct nodo *left;
+    struct nodo *right;
+    struct nodo *parent;
+    int color;
+} nodo;
 
-int left(int i){
-    return 2*i;
-}
+typedef struct Tree{
+    nodo *root;
+    nodo *nil;
+} Tree;
 
-int right(int i){
-    return 2*i+1;
-}
+typedef struct strPesi{
+    int indice;
+    int peso;
+}strPesi
+;
 
+int dijkstra(int numVert,int G[numVert][numVert],int sorgente);
 
-void maxHeapify(strDistMin arrStrDist[], int i, int length){
-    int l= left(i);
-    int r= right(i);
-    int tmp,tmp2;
-    int max;
-    if ((l <= length) && (arrStrDist[l].SomDistMin > arrStrDist[i].SomDistMin))
-        max = l;
-    else
-        max=i;
+void rb_insert(Tree* T,int somDistMin,int indiceGraf );
 
-    if ((r<= length) && (arrStrDist[r].SomDistMin > arrStrDist[max].SomDistMin))
-        max=r;
+void rb_insert_fixup(Tree* T, nodo * z);
 
-    if (max!= i) {
-        tmp=arrStrDist[i].SomDistMin;
-        tmp2=arrStrDist[i].indiceGraf;
-        arrStrDist[i].SomDistMin=arrStrDist[max].SomDistMin;
-        arrStrDist[i].indiceGraf=arrStrDist[max].indiceGraf;
+void left_rotate( Tree* T,nodo* x );
 
-        arrStrDist[max].SomDistMin=tmp;
-        arrStrDist[max].indiceGraf=tmp2;
+void right_rotate(Tree* T,nodo *x);
 
-        maxHeapify(arrStrDist, max, length);
-    }
-}
+void stampa(struct nodo* da_stampare);
 
-void buildMaxHeap(strDistMin arrStrDist[], int length){
+void stampaCort(struct nodo* da_stampare,int maxDaStampare, int*stampati);
 
-    for (int i= length/2; i>=0; i--) {
-        maxHeapify(arrStrDist, i, length);
-    }
-}
+void buildMinHeap(strPesi stPesi[], int length);
 
-void heapsort(strDistMin arrStrDist[], int length){
-    buildMaxHeap(arrStrDist, length);
-    for (int i= length; i>0; i--) {
-        int tmp=arrStrDist[i].SomDistMin;
-        int tmp2=arrStrDist[i].indiceGraf;
-        arrStrDist[i].SomDistMin=arrStrDist[0].SomDistMin;
-        arrStrDist[i].indiceGraf=arrStrDist[0].indiceGraf;
-        arrStrDist[0].SomDistMin=tmp;
-        arrStrDist[0].indiceGraf=tmp2;
-        length--;
-        maxHeapify(arrStrDist, 0, length);
-    }
-}
+void minHeapify(strPesi stPesi[], int i, int length);
 
-int dijkstra (int numVert, int G[numVert][numVert], int sorgente ){
-    int dist[numVert];
-    int visited[numVert],adj=0;
+void enqueue(strPesi stPesi[], int peso,int indice, int *dimPQ);
 
-    for(int vr=0; vr < numVert; vr++){
-        for(int ed=0; ed < numVert; ed++){
-            if(G[vr][ed]==0)
-                G[vr][ed]=INFINITY;
-        }
-        dist[vr]=G[sorgente][vr];
-        visited[vr]=0;
-    }
-    dist[sorgente]=0;
-    visited[sorgente]=1;
+void swap(strPesi *str1, strPesi *str2);
 
-    int shortestpath;
-    for(int passo=1; passo < numVert - 1; passo++)
-    {
-        shortestpath=INFINITY;
-        for(int v=0; v < numVert; v++)
-            if(shortestpath >= dist[v] && visited[v] == 0)
-            {
-                shortestpath=dist[v];
-                adj=v;
-            }
-        visited[adj]=1;
-            for(int v=0; v < numVert; v++)
-                if(visited[v]==0 && dist[v] > shortestpath + G[adj][v])
-                    dist[v]= shortestpath + G[adj][v];
-    }
-    int sumOfDist =0;
-    for(int v=0; v < numVert; v++)
-        if(v!=sorgente)
-        {
-            if(dist[v]==INFINITY)
-                dist[v]=0;
-            sumOfDist+=dist[v];
-        }
-    return sumOfDist;
-}
+void deQueue(strPesi stPesi[],int *dimPQ);
 
+void sort(strPesi stPesi[], int dimQ);
 
 int main() {
 
     //FILE *fp;
-    //fp = fopen("input_3", "r");
+    //fp = fopen("input_1", "r");
     //if (fp == NULL) {
-    //    perror("Unable to open file!");
-    //    exit(1);
+      //  perror("Unable to open file!");
+       // exit(1);
     //}
 
-    char ** lines ;
-    char *primaLinea;
-    int num_linee = 0;
-    int lungStrin =LUNG_MAX_STRING ;
-    char buf[MAX_BUF];
-    int numNodi;
-    int lungClassific;
-
-
-                                                             // while (fgets(buf, lungStrin, stdin)!=null;    per dopo quando usero input da stdinput
-    primaLinea = (char *)malloc(sizeof(buf)+1);
-    char *fgetsReturn1 = fgets(primaLinea, lungStrin, stdin);
-            if(fgetsReturn1!=NULL){};                                                 // scanf("%s",&primaLinea);
-                                                                   //strncpy(primaLinea, buf,MAX_BUF+1);
-    if (strncmp(primaLinea, "Topk\n", LUNG_MAX_STRING) == 0) {
-        printf("\n");
-        char *fgetsReturn2 = fgets(primaLinea, lungStrin, stdin);                                                          // while (fgets(buf, lungStrin, stdin)!=null;    per dopo quando usero input da stdinput
-        if(fgetsReturn2!=NULL){};                                           // scanf("%s",&primaLinea);
-                                                             //strncpy(primaLinea, buf,MAX_BUF);
-    }
     char *noNod, *lungcl;
-    noNod = strtok(primaLinea, " ");
+    char buf[MAX_BUF];
+    int numNodi,lungClassific;
+    int grafIndex=0;
+
+    nodo nil = { 0,  0,  NULL, NULL, NULL, BLACK};
+    Tree* TdiGrad;
+    Tree Tgrad;
+    TdiGrad=& Tgrad;
+    Tgrad.nil = &nil;
+    Tgrad.root = &nil;
+
+    char* buffer = (char *)malloc(sizeof(buf)+1);
+    char *fgetsReturn1 = fgets(buffer, LUNG_MAX_STRING, stdin);
+    if(fgetsReturn1!=NULL){};
+    if (strncmp(buffer, "Topk\n", 1) == 0) {
+        printf("\n");
+        char *fgetsReturn2 = fgets(buffer, LUNG_MAX_STRING, stdin);
+        if(fgetsReturn2!=NULL){};
+    }
+    noNod = strtok(buffer, " ");
     numNodi = atoi(noNod);
     lungcl = strtok(NULL, " ");
     lungClassific= atoi(lungcl);
-    lines = (char **) malloc(N_MAX_LINEE * sizeof(char *));
-
-
-    while (fgets(buf, lungStrin, stdin) != NULL) {                   // while (fgets(buf, lungStrin, stdin)!=null;    per dopo quando usero input da stdinput
-        lines[num_linee] = (char *)malloc(sizeof(buf)+1);
-        strncpy(lines[num_linee], buf,MAX_BUF);
-        num_linee++;
-    }
-
     int  grafo[numNodi][numNodi];
-    char *richiesta;
-    int numIndDaStampare;
-    int grafIndex=0;
-    strDistMin ArrStrdistMIn[MAXGRAF];
-    ArrStrdistMIn->indiceGraf=0;
-    ArrStrdistMIn->SomDistMin=0;
-    int j=0;
-    while(j<num_linee){
-        richiesta = lines[j];
-        if (strncmp(richiesta, "TopK", LUNG_MAX_STRING) == 0||strncmp(richiesta, "TopK\n", LUNG_MAX_STRING) == 0) {
+
+    while (fgets(buffer, 100, stdin) != NULL  ) {
+
+        if (strncmp(buffer, "TopK", 1) == 0) {
             if(grafIndex==0){
                 printf("\n");
             }
             else{
-                heapsort(ArrStrdistMIn, grafIndex - 1);
-                if (lungClassific > grafIndex)
-                    numIndDaStampare=grafIndex;
-                else
-                    numIndDaStampare=lungClassific;
-
-                for (int i = 0; i < numIndDaStampare; i++){
-                    printf(" %d",ArrStrdistMIn[i].indiceGraf);
+                if (lungClassific < grafIndex){
+                    int stampati=0;
+                    stampaCort(TdiGrad->root,lungClassific,&stampati);
+                }
+                else{
+                    stampa(TdiGrad->root);
                 }
                 printf("\n");
             }
-            j++;
         }
-        else if (strncmp(richiesta, "AggiungiGrafo\n", LUNG_MAX_STRING) == 0) {
+        else if (strncmp(buffer, "AggiungiGrafo\n", 1) == 0) {
 
             for (int i = 0; i < numNodi ; i++){
-                j++;
                 int k=0;
-                char *stringPesi = strtok(lines[j], ",");
-                int peso= atoi(stringPesi);
-                grafo[i][k]=peso;
-                while(stringPesi != NULL && k < numNodi ){
+                char *fgetsReturn3 = fgets(buffer, LUNG_MAX_STRING, stdin);
+                if(fgetsReturn3){};
+                int dig=0;
+                int resultFin=0;
+                strToINt(dig,buffer,resultFin)
+                grafo[i][k]= resultFin;
+                dig++;
+                while(k < numNodi ){
                     k++;
-                    stringPesi = strtok(NULL, ",");
-                    if(stringPesi != NULL){
-                        peso= atoi(stringPesi);
-                        grafo[i][k]=peso;
-                    }
+                    resultFin=0;
+                    strToINt(dig,buffer,resultFin)
+                    grafo[i][k]= resultFin;
+                    dig++;
                 }
             }
             int sumOFDist = dijkstra(numNodi, grafo, 0);
-            //printf("\n%d",sumOFDist);
-            ArrStrdistMIn[grafIndex].indiceGraf=grafIndex;
-            ArrStrdistMIn[grafIndex].SomDistMin=sumOFDist;
-            ArrStrdistMIn[grafIndex].graduatoria=grafIndex;
-
+            rb_insert(TdiGrad,sumOFDist,grafIndex);
             grafIndex++;
-            j++;
         }
-
     }
-    for (int i = 0; i < num_linee; i++) {
-        free(lines[i]);
-    }
-    free(primaLinea);
-    free(lines);
+    free(buffer);
     //fclose(fp);
-    //printf("\nho finito\n\n");
+
     return 0;
 }
 
 
+int dijkstra (int numVert, int G[numVert][numVert], int sorgente ){
+    int dimQ=0;
+    int u, uDist;
+    strPesi Qpesi[numVert];
+    for(int v=0; v < numVert; v++){
+        for(int ed=0; ed < numVert; ed++){
+            if(G[v][ed]==0)
+                G[v][ed]=INFINITY;
+        }
+        enqueue(Qpesi, G[sorgente][v], v, &dimQ);
+    }
+    Qpesi[sorgente].peso=0;
+    deQueue(Qpesi, &dimQ);
 
+    while(dimQ > 0){
+        int i=1;
+        if(numVert>4)
+            buildMinHeap(Qpesi,dimQ);
+        else sort(Qpesi, dimQ);
+
+        u=Qpesi[0].indice;
+        uDist=Qpesi[0].peso;
+        if(uDist <INFINITY){
+            while(i < dimQ){
+                int secRoute= uDist + G[u][Qpesi[i].indice];
+                if(secRoute <= Qpesi[i].peso){
+                    Qpesi[i].peso= secRoute;
+                }
+                i++;
+            }
+        }
+        deQueue(Qpesi, &dimQ);
+    }
+    int sumOfDist =0;
+    for(int v=0; v < numVert-1; v++)
+        if(Qpesi[v].indice != sorgente )
+        {
+            if(Qpesi[v].peso >= INFINITY)
+                Qpesi[v].peso=0;
+            sumOfDist+=Qpesi[v].peso;
+        }
+    return sumOfDist;
+}
+
+void left_rotate( Tree* T,nodo* x ){
+    struct nodo *y = x->right;
+    x->right = y->left;
+
+    if (y->left != T->nil) {
+        y->left->parent = x;
+    }
+    y->parent = x->parent;
+    if (x->parent == T->nil) {
+        T->root = y;
+    }
+    else if (x == x->parent->left) {
+        x->parent->left = y;
+    }
+    else {
+        x->parent->right = y;
+    }
+    y->left = x;
+    x->parent = y;
+}
+
+void right_rotate(Tree* T,struct nodo *x){
+    nodo *y = x->left;
+    x->left = y->right;
+    if (y->right != T->nil) {
+        y->right->parent = x;
+    }
+    y->parent = x->parent;
+
+    if (x->parent == T->nil) {
+        T->root = y;
+    }
+    else if (x == x->parent->right){
+        x->parent->right = y;
+    }
+    else{
+        x->parent->left = y;
+    }
+    y->right = x;
+    x->parent = y;
+}
+
+void rb_insert_fixup(Tree* T, nodo * z){
+
+    struct nodo *y;
+    if (z == T->root)
+    {
+        T->root->color= BLACK;
+    }
+    else
+    {
+        nodo* x = z->parent;
+        if (x->color == RED)
+        {
+            if (x == x->parent->left)
+            {
+
+                y = x->parent->right;
+                if (y->color == RED)
+                {
+                    x->color = BLACK;
+                    y->color = BLACK;
+                    x->parent->color = RED;
+                    rb_insert_fixup(T,x->parent);
+                }
+                else
+                {
+                    if (z == x->right)
+                    {
+                        z = x;
+                        left_rotate(T,z);
+                        x = z->parent;
+                    }
+                    x->color = BLACK;
+                    x->parent->color = RED;
+                    right_rotate(T,x->parent);
+                }
+            }
+            else
+            {
+                y = x->parent->left;
+                if (y->color == RED)
+                {
+                    x->color = BLACK;
+                    y->color = BLACK;
+                    x->parent->color = RED;
+                    rb_insert_fixup(T,x->parent);
+                }
+                else
+                {
+                    if (z == x->left)
+                    {
+                        z = x;
+                        right_rotate(T,z);
+                        x = z->parent;
+                    }
+                    x->color = BLACK;
+                    x->parent->color = RED;
+                    left_rotate(T,x->parent);
+                }
+            }
+        }
+
+    }
+}
+
+void rb_insert(Tree* T,int somDistMin,int indiceGraf ){
+
+    struct nodo* z = (struct nodo*)malloc(sizeof(struct nodo));
+    z->SomDistMin = somDistMin;
+    z->indiceGraf = indiceGraf;
+    z->left= NULL ;
+    z->right= NULL;
+    z->parent= NULL;
+    z->color=RED;
+
+    struct nodo *y = T->nil;
+    struct nodo *x = T->root;
+
+    while (x != T->nil)
+    {
+        y = x;
+        if (z->SomDistMin < x->SomDistMin)
+        {
+            x = x->left;
+        }
+        else
+        {
+            x = x->right;
+        }
+    }
+
+    z->parent = y;
+    if (y == T->nil)
+    {
+        T->root = z;
+    }
+    else if (z->SomDistMin<y->SomDistMin)
+    {
+        y->left = z;
+    }
+    else
+    {
+        y->right = z;
+    }
+    z->left = T->nil;
+    z->right = T->nil;
+    z->color = RED;
+    rb_insert_fixup(T,z);
+}
+
+
+void stampa(nodo* da_stampare){
+    if(da_stampare->right != NULL){
+        stampa(da_stampare->left);
+        printf(" %d",da_stampare->indiceGraf);
+        stampa(da_stampare->right);
+    }
+}
+
+void stampaCort(nodo* da_stampare, int maxDaStampare, int*stampati){
+
+    if(da_stampare->right == NULL || *stampati >= maxDaStampare){
+        return;
+    }
+    if(da_stampare->left != NULL){
+        stampaCort(da_stampare->left,maxDaStampare,stampati);
+    }
+    if(*stampati < maxDaStampare){
+        printf(" %d",da_stampare->indiceGraf);
+        (*stampati)++;
+    }
+    stampaCort( da_stampare->right,maxDaStampare,stampati);
+}
+
+void enqueue(strPesi stPesi[], int peso,int indice,int* puntDimPQ)
+{
+    if (*puntDimPQ==0)
+    {
+        stPesi[0].indice= indice;
+        stPesi[0].peso = peso;
+        *puntDimPQ= *puntDimPQ+1;
+    }
+    else
+    {
+        stPesi[*puntDimPQ].peso = peso;
+        stPesi[*puntDimPQ].indice= indice;
+        *puntDimPQ= *puntDimPQ+1;
+    }
+}
+
+void swap(strPesi *str1, strPesi *str2)
+{
+    strPesi temp = *str2;
+    *str2 = *str1;
+    *str1 = temp;
+}
+
+void deQueue(strPesi stPesi[],int* dimPQ)
+{
+    swap(&stPesi[0], &stPesi[*dimPQ - 1]);
+    *dimPQ = *dimPQ-1;
+}
+
+void minHeapify(strPesi stPesi[], int i, int length){
+    int lft= 2*i;
+    int rgt= 2*i+1;
+    int min;
+
+    if (lft < length && stPesi[lft].peso < stPesi[i].peso)
+        min = lft;
+    else
+        min=i;
+
+    if (rgt< length && stPesi[rgt].peso< stPesi[min].peso)
+        min=rgt;
+
+    if (min!= i) {
+        swap(&stPesi[i],&stPesi[min]);
+        minHeapify(stPesi, min, length);
+    }
+}
+
+void buildMinHeap(strPesi stPesi[], int length){
+
+    for (int i= length/2; i>=0; i--) {
+        minHeapify(stPesi, i, length);
+    }
+}
+
+void sort(strPesi stPesi[], int dimQ){
+
+    if (dimQ>1){
+        if(stPesi[0].peso>stPesi[1].peso || (stPesi[0].peso==stPesi[1].peso&&stPesi[0].indice > stPesi[1].indice))
+        swap(&stPesi[0],&stPesi[1]);
+        if(dimQ>2){
+            if(stPesi[1].peso>stPesi[2].peso || (stPesi[1].peso==stPesi[2].peso&&stPesi[1].indice > stPesi[2].indice))
+                swap(&stPesi[1],&stPesi[2]);
+        }
+    }
+}
